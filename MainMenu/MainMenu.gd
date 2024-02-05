a7a57f28e95450b6efcb2ad11577febb8780889d
Jaimemilio <105794884+KonigJakob@users.noreply.github.com>
2024-02-05 17:57:07 +0100

    Add Scenes and prototype

---
+++ MainMenu/MainMenu.gd
@@ -0,0 +1,11 @@
+extends Control
+
+
+
+func _on_button_famous_guests_pressed():
+	get_tree().change_scene_to_file("res://FamousGuests/FamousGuests.tscn")
+
+
+
+func _on_button_guest_book_pressed():
+	get_tree().change_scene_to_file("res://GuestBook/GuestBook.tscn")
