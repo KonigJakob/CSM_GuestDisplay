c7f0408672057f5187e566980adc2c97e8eb5efe
Jaimemilio <105794884+KonigJakob@users.noreply.github.com>
2024-02-20 10:57:13 +0100

    Implement first version scrolling

--- FamousGuests/FamousGuests.gd
+++ FamousGuests/FamousGuests.gd
@@ -8,23 +8,28 @@ var guest_portrait = preload("res://FamousGuests/guest_scene.tscn")
 @export var portrait_position_start : Vector2
 @export var portrait_gutter: float
 
-var current_portrait = 1
+var right_clicks : int
+var left_clicks : int
 
 func _ready():
 	guests = load_guests()
-	carousel_guests()
-	print(visible_guests)
-	set_guest_positions(visible_guests, $Panel)
+	set_guest_positions(guests, $Panel)
+	right_clicks = 0
+	left_clicks = guests.size()
 
 func _process(delta):
-	if Input.is_action_just_pressed("move_right"):
+	if Input.is_action_just_pressed("move_right") and right_clicks > 0:
 		tween = get_tree().create_tween().set_parallel(true)
-		for g in visible_guests:
+		for g in guests:
 			move_guests_left(g)
-	if Input.is_action_just_pressed("move_left"):
+		right_clicks -= 1
+		left_clicks += 1
+	if Input.is_action_just_pressed("move_left") and left_clicks > 0:
 		tween = get_tree().create_tween().set_parallel(true)
-		for g in visible_guests:
+		for g in guests:
 			move_guests_right(g)
+		right_clicks += 1
+		left_clicks -= 1
 
 func load_guests() -> Array:
 	var loaded_guests = []
@@ -42,27 +47,17 @@ func load_guests() -> Array:
 			_guest_portrait.famous_for = guests[g]["Famous for"]
 			_guest_portrait.image_1 = guests[g]["Image 1"]
 			loaded_guests.append(_guest_portrait)
-		print("Guests loaded")
 	else: 
 		print("Couldn't load guests.")
 	return loaded_guests
 
 func set_guest_positions(guest_array : Array, parent):
-	print("Setting position")
 	var current_x : int = 0
 	for g in guest_array:
 		#g.gloabl_position.y = parent.position.y
 		g.position.x = current_x
 		current_x += g.size.x + portrait_gutter
-		if g.position.x == parent.position.x:
-			current_portrait = g
-
-func carousel_guests():
-	for i in 5:
-		var new_visible_guest = guests.pop_front()
-		visible_guests.append(new_visible_guest)
-		#You need to add the children at some point but not necessarily here. Good luck! Love you :
-		#$Panel.add_child(new_visible_guest)
+		parent.add_child(g)
 
 func move_guests_right(g):
 	tween.tween_property(g, "position", Vector2(g.position.x - g.size.x - portrait_gutter, g.position.y), 1)
