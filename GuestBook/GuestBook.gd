4c28b9a50ce0818e489468399c63844d05addfee
Jaimemilio <105794884+KonigJakob@users.noreply.github.com>
2024-02-22 18:00:43 +0100

    improve guest book navigation

--- GuestBook/GuestBook.gd
+++ GuestBook/GuestBook.gd
@@ -21,6 +21,8 @@ enum PollPage {
 
 @export var forward_button: Button
 @export var back_button: Button
+@export var tween_wait_interval: float
+@export var tween_movement_interval: float
 @export var submit_button: Button
 
 @export var panels : HBoxContainer
@@ -49,56 +51,66 @@ func _on_star_1_pressed():
 	Star_Three.button_pressed = false
 	Star_Four.button_pressed = false
 	Star_Five.button_pressed = false
+	tween_forward()
 func _on_star_2_pressed():
 	satisfaction_button = 2
 	Star_One.button_pressed = true
 	Star_Three.button_pressed = false
 	Star_Four.button_pressed = false
 	Star_Five.button_pressed = false
+	tween_forward()
 func _on_star_3_pressed():
 	satisfaction_button = 3
 	Star_One.button_pressed = true
 	Star_Two.button_pressed = true
 	Star_Four.button_pressed = false
 	Star_Five.button_pressed = false
+	tween_forward()
 func _on_star_4_pressed():
 	satisfaction_button = 4
 	Star_One.button_pressed = true
 	Star_Two.button_pressed = true
 	Star_Three.button_pressed = true
 	Star_Five.button_pressed = false
+	tween_forward()
 func _on_star_5_pressed():
 	satisfaction_button = 5
 	Star_One.button_pressed = true
 	Star_Two.button_pressed = true
 	Star_Three.button_pressed = true
 	Star_Four.button_pressed = true
+	tween_forward()
 
 func _on_button_child_pressed():
 	age_group_button = 1
 	Age_Teen.button_pressed = false
 	Age_Adult.button_pressed = false
 	Age_Senior.button_pressed = false
+	tween_forward()
 func _on_button_teen_pressed():
 	age_group_button = 2
 	Age_Child.button_pressed = false
 	Age_Adult.button_pressed = false
 	Age_Senior.button_pressed = false
+	tween_forward()
 func _on_button_adult_pressed():
 	age_group_button = 3
 	Age_Child.button_pressed = false
 	Age_Teen.button_pressed = false
 	Age_Senior.button_pressed = false
+	tween_forward()
 func _on_button_senior_pressed():
 	age_group_button = 4
 	Age_Child.button_pressed = false
 	Age_Teen.button_pressed = false
 	Age_Adult.button_pressed = false
+	tween_forward()
 
 func _on_option_button_item_selected(index):
 	language = index
+	tween_forward()
 
-func _on_line_edit_text_changed(new_text):
+func _on_text_changed(new_text):
 	message = new_text
 	submit_button.visible = true
 
@@ -109,33 +121,27 @@ func _on_button_yes_pressed():
 	get_tree().reload_current_scene()
 
 func _on_button_return_pressed():
-	var panel_size = panels.size/panels.get_child_count()
-	var tween = get_tree().create_tween()
-	tween.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_QUART)
-	tween.tween_property(panels, "position", Vector2(panels.position.x + panel_size.x, panels.position.y),1)
-	current_page -= 1
-	match current_page: 
-		PollPage.STARS:
-			back_button.visible = false
-			forward_button.visible = true
-			submit_button.visible = false
-		PollPage.AGE:
-			back_button.visible = true
-			forward_button.visible = true
-			submit_button.visible = false
-		PollPage.LANGUAGE:
-			back_button.visible = true
-			forward_button.visible = true
-			submit_button.visible = false
-		PollPage.MESSAGE:
-			back_button.visible = true
-			forward_button.visible = false
+	move_page_backwards()
 
 func _on_button_continue_pressed():
+	move_page_forward()
+
+func _on_button_home_pressed():
+	get_tree().change_scene_to_file("res://MainMenu/main.tscn")
+
+func _on_button_submit_pressed():
+	poll_data = {"Date" : date, "Satisfaction": satisfaction_button, 
+	"Age Group" : age_group_button, "Language" : language, "Message" : message}
+	JSON_string = JSON.stringify(poll_data)
+	SaveSystem.save(JSON_string)
+	add_child(promt_panel)
+	promt_panel.global_position = Vector2.ZERO
+
+func move_page_forward():
 	var panel_size = panels.size/panels.get_child_count()
 	var tween = get_tree().create_tween()
 	tween.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_QUART)
-	tween.tween_property(panels, "position", Vector2(panels.position.x - panel_size.x, panels.position.y),1)
+	tween.tween_property(panels, "position", Vector2(panels.position.x - panel_size.x, panels.position.y),tween_movement_interval)
 	current_page += 1
 	match current_page: 
 		PollPage.STARS:
@@ -153,14 +159,30 @@ func _on_button_continue_pressed():
 		PollPage.MESSAGE:
 			back_button.visible = true
 			forward_button.visible = false
-
-func _on_button_home_pressed():
-	get_tree().change_scene_to_file("res://MainMenu/main.tscn")
-
-func _on_button_submit_pressed():
-	poll_data = {"Date" : date, "Satisfaction": satisfaction_button, 
-	"Age Group" : age_group_button, "Language" : language, "Message" : message}
-	JSON_string = JSON.stringify(poll_data)
-	SaveSystem.save(JSON_string)
-	add_child(promt_panel)
-	promt_panel.global_position = Vector2.ZERO
+func move_page_backwards():
+	var panel_size = panels.size/panels.get_child_count()
+	var tween = get_tree().create_tween()
+	tween.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_QUART)
+	tween.tween_property(panels, "position", Vector2(panels.position.x + panel_size.x, panels.position.y),tween_movement_interval)
+	current_page -= 1
+	match current_page: 
+		PollPage.STARS:
+			back_button.visible = false
+			forward_button.visible = true
+			submit_button.visible = false
+		PollPage.AGE:
+			back_button.visible = true
+			forward_button.visible = true
+			submit_button.visible = false
+		PollPage.LANGUAGE:
+			back_button.visible = true
+			forward_button.visible = true
+			submit_button.visible = false
+		PollPage.MESSAGE:
+			back_button.visible = true
+			forward_button.visible = false
+			submit_button.visible = true
+func tween_forward():
+	var tween = get_tree().create_tween()
+	tween.tween_interval(tween_wait_interval)
+	tween.tween_callback(move_page_forward)
