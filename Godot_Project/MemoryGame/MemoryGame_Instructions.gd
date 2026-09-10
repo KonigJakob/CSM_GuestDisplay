40b6a2a6659aa7ea8743fbc5e6ae55dba238302e
Jaimemilio <105794884+KonigJakob@users.noreply.github.com>
2026-09-10 10:39:36 +0200

    add instructions tween

--- Godot_Project/MemoryGame/MemoryGame_Instructions.gd
+++ Godot_Project/MemoryGame/MemoryGame_Instructions.gd
@@ -5,10 +5,10 @@ var timer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	timer = get_tree().create_timer(3)
+	timer = get_tree().create_timer(1)
 	timer.timeout.connect(on_timer_timout)
 
 func on_timer_timout():
 	var tween = get_tree().create_tween()
-	tween.set_loops(1).set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_CIRC)
-	add_theme_font_size_override("font_size", 10)
+	tween.set_loops(1).set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_BACK)
+	tween.tween_property(self, "theme_override_font_sizes/font_size", 50, 1)
